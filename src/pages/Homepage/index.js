213d9c42b201a9c0b8dfd7bc1bfa0e8c14b1e27e
dahliacreative <simon@dahliacreative.com>
2019-10-10 16:03:36 +0100

    Homepage meta

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { hooks } from 'settings'
 import Container from 'components/Container'
 import Banner from 'components/Banner'
 
 import NewsPartial from './news'
 
 const Homepage = () => {
+    hooks.useMeta('FLHS :: Home')
     return (
         <>
             <Banner id="2czKBE4gEnYEgEJasstKMP" />
