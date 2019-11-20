a87d68f92628cab2cc1b39ef483c334883f367f7
dahliacreative <simon@dahliacreative.com>
2019-11-20 11:30:01 +0000

    Committee member ordeR

--- src/pages/Society/committee.js
+++ src/pages/Society/committee.js
@@ -1,52 +1,60 @@
-import React from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { useBreakpoints } from 'react-device-breakpoints'
-import { constants, hooks } from 'settings'
-import { gql } from 'apollo-boost'
-import Member, { MemberFragment } from 'components/Member'
-import Loading from 'components/Loading'
-import Error from 'components/Error'
-import Container from 'components/Container'
-import Grid from 'components/Grid'
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { useBreakpoints } from "react-device-breakpoints";
+import { constants, hooks } from "settings";
+import { gql } from "apollo-boost";
+import Member, { MemberFragment } from "components/Member";
+import Loading from "components/Loading";
+import Error from "components/Error";
+import Container from "components/Container";
+import Grid from "components/Grid";
 
 const query = gql`
-    ${MemberFragment}
-    query CommitteeMembers($transform: ImageTransformOptions) {
-        committeeMemberCollection(order: sys_publishedAt_ASC) {
-            items {
-                ...MemberFragment
-            }
-        }
+  ${MemberFragment}
+  query CommitteeMembers($transform: ImageTransformOptions) {
+    committeeMemberCollection(order: position_ASC) {
+      items {
+        ...MemberFragment
+      }
     }
-`
+  }
+`;
 
 const Committee = () => {
-    hooks.useMeta('FLHS :: Society :: Committee')
-    const device = useBreakpoints()
-    const { loading, error, data } = useQuery(query, {
-        variables: { transform: constants.PROFILE_IMAGE_DIMENSIONS }
-    })
-    return (
-        <Container light pad>
-            {error ? (
-                <Error error={error} />
-            ) : (
-                <>
-                    {loading ? (
-                        <Loading />
-                    ) : (
-                        <Grid columns={device.isMobile ? 1 : device.isLargeMobile || device.isTablet ? 2 : 3}>
-                            {data.committeeMemberCollection.items.map(member => (
-                                <Grid.Item key={member.sys.id}>
-                                    <Member {...member} />
-                                </Grid.Item>
-                            ))}
-                        </Grid>
-                    )}
-                </>
-            )}
-        </Container>
-    )
-}
+  hooks.useMeta("FLHS :: Society :: Committee");
+  const device = useBreakpoints();
+  const { loading, error, data } = useQuery(query, {
+    variables: { transform: constants.PROFILE_IMAGE_DIMENSIONS }
+  });
+  return (
+    <Container light pad>
+      {error ? (
+        <Error error={error} />
+      ) : (
+        <>
+          {loading ? (
+            <Loading />
+          ) : (
+            <Grid
+              columns={
+                device.isMobile
+                  ? 1
+                  : device.isLargeMobile || device.isTablet
+                  ? 2
+                  : 3
+              }
+            >
+              {data.committeeMemberCollection.items.map(member => (
+                <Grid.Item key={member.sys.id}>
+                  <Member {...member} />
+                </Grid.Item>
+              ))}
+            </Grid>
+          )}
+        </>
+      )}
+    </Container>
+  );
+};
 
-export default Committee
+export default Committee;
