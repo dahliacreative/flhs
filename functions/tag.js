e136404c074e71ae9343a7e0def814293584f5a4
dahliacreative <simon@dahliacreative.com>
2019-11-27 10:39:49 +0000

    Updated lambda to link tag to record

--- functions/tag.js
+++ functions/tag.js
@@ -4,33 +4,41 @@ const client = createClient({
   accessToken: process.env.CMS_TOKEN
 })
 
-exports.handler = (event, context, callback) => {
-  const data = JSON.parse(event.body)
-  client
-    .getSpace(process.env.REACT_APP_SPACE_ID)
-    .then(space => space.getEnvironment(process.env.ENV_ID))
-    .then(environment =>
-      environment.createEntry(process.env.CONTENT_TYPE_ID, {
-        fields: {
-          title: {
-            'en-US': data.title
-          },
-          tagData: {
-            'en-US': data.tagData
-          }
+exports.handler = async (event, context, callback) => {
+  try {
+    const data = JSON.parse(event.body)
+    const space = await client.getSpace(process.env.REACT_APP_SPACE_ID)
+    const environment = await space.getEnvironment(process.env.ENV_ID)
+    const record = await environment.getEntry(data.recordId)
+    const entry = await environment.createEntry(process.env.CONTENT_TYPE_ID, {
+      fields: {
+        title: {
+          'en-US': data.title
+        },
+        tagData: {
+          'en-US': data.tagData
         }
-      })
-    )
-    .then(entry => {
-      entry.publish()
-      callback(null, {
-        statusCode: 200
-      })
+      }
     })
-    .catch(error => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(error)
-      })
+    entry.publish()
+    record.fields.imageTags['en-US'] = [
+      ...record.fields.imageTags['en-US'],
+      {
+        sys: {
+          id: entry.sys.id,
+          linkType: 'Entry',
+          type: 'Link'
+        }
+      }
+    ]
+    await record.update()
+    callback(null, {
+      statusCode: 200
     })
+  } catch (error) {
+    callback(null, {
+      statusCode: 422,
+      body: JSON.stringify(error)
+    })
+  }
 }
