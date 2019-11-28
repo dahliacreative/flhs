30d1ea33f505a927a457eff818ab70dc8d59c639
dahliacreative <simon@dahliacreative.com>
2019-11-28 22:23:47 +0000

    record order

--- src/pages/Archives/queries.js
+++ src/pages/Archives/queries.js
@@ -2,57 +2,57 @@ import { gql } from 'apollo-boost'
 import { RecordFragment } from 'components/Record'
 
 const category = gql`
-    ${RecordFragment}
-    query Records($transform: ImageTransformOptions!, $categoryId: String!, $skip: Int!, $limit: Int!) {
-        category(id: $categoryId) {
-            banner {
-                sys {
-                    id
-                }
-            }
-            linkedFrom {
-                recordCollection(skip: $skip, limit: $limit) {
-                    skip
-                    limit
-                    total
-                    items {
-                        ...RecordFragment
-                    }
-                }
-            }
+  ${RecordFragment}
+  query Records($transform: ImageTransformOptions!, $categoryId: String!, $skip: Int!, $limit: Int!) {
+    category(id: $categoryId) {
+      banner {
+        sys {
+          id
         }
+      }
+      linkedFrom {
+        recordCollection(skip: $skip, limit: $limit, order: sys_firstPublishedAt_DESC) {
+          skip
+          limit
+          total
+          items {
+            ...RecordFragment
+          }
+        }
+      }
     }
+  }
 `
 
 const archive = gql`
-    ${RecordFragment}
-    query Records($transform: ImageTransformOptions!, $skip: Int!, $limit: Int!, $order: [RecordOrder]!) {
-        recordCollection(skip: $skip, limit: $limit, order: $order) {
-            skip
-            limit
-            total
-            items {
-                ...RecordFragment
-            }
-        }
+  ${RecordFragment}
+  query Records($transform: ImageTransformOptions!, $skip: Int!, $limit: Int!, $order: [RecordOrder]!) {
+    recordCollection(skip: $skip, limit: $limit, order: $order) {
+      skip
+      limit
+      total
+      items {
+        ...RecordFragment
+      }
     }
+  }
 `
 
 const categories = gql`
-    query Categories {
-        categoryCollection {
-            items {
-                title
-                sys {
-                    id
-                }
-            }
+  query Categories {
+    categoryCollection {
+      items {
+        title
+        sys {
+          id
         }
+      }
     }
+  }
 `
 
 export default {
-    category,
-    archive,
-    categories
+  category,
+  archive,
+  categories
 }
