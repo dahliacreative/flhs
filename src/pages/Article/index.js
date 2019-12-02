f5a14141cf54c6568126eed89cb7f4ac37e72fd5
dahliacreative <simon@dahliacreative.com>
2019-12-02 23:35:47 +0000

    Article image render

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -13,88 +13,97 @@ import Banner from 'components/Banner'
 import Author from 'components/Author'
 import styles from './styles.module.sass'
 
-const query = gql`
-    ${ArticleFragment}
-    query Article($id: String!, $transform: ImageTransformOptions!, $avatar: ImageTransformOptions!) {
-        article(id: $id) {
-            ...ArticleFragment
-            content {
-                json
-            }
-        }
+const options = {
+  renderNode: {
+    'embedded-asset-block': node => {
+      const file = node.data.target.fields.file
+      return <img className="fluid" src={file.url} alt="" />
     }
+  }
+}
+
+const query = gql`
+  ${ArticleFragment}
+  query Article($id: String!, $transform: ImageTransformOptions!, $avatar: ImageTransformOptions!) {
+    article(id: $id) {
+      ...ArticleFragment
+      content {
+        json
+      }
+    }
+  }
 `
 
 const Article = ({ match, history }) => {
-    const [title, updateTitle] = useState('FLHS :: News')
-    hooks.useMeta(title)
-    const {
-        loading,
-        error,
-        data: { article }
-    } = useQuery(query, {
-        variables: {
-            id: match.params.id,
-            transform: constants.BANNER_IMAGE_DIMENSIONS,
-            avatar: constants.AVATAR_IMAGE_DIMENSIONS
-        },
-        onCompleted: data => {
-            updateTitle(`FLHS :: News :: ${data.article.title}`)
-        }
-    })
-    hooks.useMeta(title)
-    return (
+  const [title, updateTitle] = useState('FLHS :: News')
+  hooks.useMeta(title)
+  const {
+    loading,
+    error,
+    data: { article }
+  } = useQuery(query, {
+    variables: {
+      id: match.params.id,
+      transform: constants.BANNER_IMAGE_DIMENSIONS,
+      avatar: constants.AVATAR_IMAGE_DIMENSIONS
+    },
+    onCompleted: data => {
+      updateTitle(`FLHS :: News :: ${data.article.title}`)
+    }
+  })
+  hooks.useMeta(title)
+  return (
+    <>
+      {error ? (
+        <Error error={error} />
+      ) : (
         <>
-            {error ? (
-                <Error error={error} />
-            ) : (
-                <>
-                    {loading ? (
-                        <Loading />
-                    ) : (
-                        <>
-                            <Banner
-                                banner={{
-                                    bannerImage: {
-                                        url: article.banner.url
-                                    }
-                                }}
-                            />
-                            <main>
-                                <Container light pad>
-                                    <div className="generic">
-                                        <h1 className={styles.title}>{article.title}</h1>
-                                        <p className={styles.date}>
-                                            Published {dayjs(article.sys.firstPublishedAt).format('ddd DD MMM YYYY')}
-                                        </p>
-                                        <Author {...article.author} />
-                                    </div>
-                                    <div
-                                        className="generic"
-                                        dangerouslySetInnerHTML={{
-                                            __html: documentToHtmlString(article.content.json)
-                                        }}
-                                    />
-                                    <div className="generic">
-                                        <div className={styles.comments}>
-                                            <DiscussionEmbed
-                                                shortname={constants.DISQUS_SHORTNAME}
-                                                config={{
-                                                    url: `${window.location.origin}/news/${article.sys.id}`,
-                                                    identifier: article.sys.id,
-                                                    title: article.title
-                                                }}
-                                            />
-                                        </div>
-                                    </div>
-                                </Container>
-                            </main>
-                        </>
-                    )}
-                </>
-            )}
+          {loading ? (
+            <Loading />
+          ) : (
+            <>
+              <Banner
+                banner={{
+                  bannerImage: {
+                    url: article.banner.url
+                  }
+                }}
+              />
+              <main>
+                <Container light pad>
+                  <div className="generic">
+                    <h1 className={styles.title}>{article.title}</h1>
+                    <p className={styles.date}>
+                      Published {dayjs(article.sys.firstPublishedAt).format('ddd DD MMM YYYY')}
+                    </p>
+                    <Author {...article.author} />
+                  </div>
+                  <div
+                    className="generic"
+                    dangerouslySetInnerHTML={{
+                      __html: documentToHtmlString(article.content.json, options)
+                    }}
+                  />
+                  <div className="generic">
+                    <div className={styles.comments}>
+                      <DiscussionEmbed
+                        shortname={constants.DISQUS_SHORTNAME}
+                        config={{
+                          url: `${window.location.origin}/news/${article.sys.id}`,
+                          identifier: article.sys.id,
+                          title: article.title
+                        }}
+                      />
+                    </div>
+                  </div>
+                </Container>
+              </main>
+            </>
+          )}
         </>
-    )
+      )}
+    </>
+  )
 }
 
 export default Article
