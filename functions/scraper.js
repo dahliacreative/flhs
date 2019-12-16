28c9369c4c268df0590c7a95fe4876c463f26382
dahliacreative <simon@dahliacreative.com>
2019-12-16 16:38:51 +0000

    scraping

--- functions/scraper.js
+++ functions/scraper.js
@@ -4,21 +4,10 @@ const axios = require('axios')
 
 exports.handler = (event, context, callback) => {
   const data = JSON.parse(event.body)
-  axios
-    .get(data.url)
-    .then(({ data }) => {
-      const $ = cheerio.load(data)
-      callback({
-        statusCode: 200,
-        body: JSON.stringify({
-          thing: 'hi'
-        })
-      })
-    })
-    .catch(err => {
-      callback({
-        statusCode: 422,
-        body: JSON.stringify(err)
-      })
-    })
+  callback({
+    statusCode: 200,
+    body: {
+      thing: 'hi!'
+    }
+  })
 }
