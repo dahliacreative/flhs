233b35fb4d671cdf1af131c082a23cb7ad72d03f
dahliacreative <simon@dahliacreative.com>
2019-12-16 16:19:19 +0000

    scraping

--- functions/scraper.js
+++ functions/scraper.js
@@ -1,27 +1,13 @@
 const rp = require('request-promise')
-const $ = require('cheerio')
+const cheerio = require('cheerio')
+const axios = require('axios')
 
-exports.handler = async (event, context, callback) => {
+exports.handler = (event, context, callback) => {
   const data = JSON.parse(event.body)
-  rp(data.url).then(html => {
-    const title = $('title', html).text()
-    const description =
-      $('meta[title="description"]', html).attr('content') || $('meta[property="og:description"]', html).attr('content')
-    const image =
-      $('meta[property="og:img"]', html).attr('src') ||
-      $('main img', html)
-        .first()
-        .attr('src') ||
-      $('img', html)
-        .first()
-        .attr('src')
-    callback({
-      statusCode: 200,
-      body: {
-        title,
-        description,
-        image
-      }
+  axios.get(data.url).then(({ data }) => {
+    const $ = cheerio.load(data)
+    callback(null, {
+      thing: 'hi!'
     })
   })
 }
