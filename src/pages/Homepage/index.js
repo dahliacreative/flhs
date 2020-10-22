2272d4d24a9760c8509983f805cced8ae6e289ba
Simon Sturgess <dahliacreative@users.noreply.github.com>
2020-10-22 11:38:53 +0100

    Update index.js

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -22,25 +22,14 @@ const Homepage = () => {
                         <div className={cx([styles.featureDetail])}>
                             <h1>Welcome to the Finedon Local History Society</h1>
                             <p>
-                                It has taken almost a year in development but here is the new Finedon Local History
-                                Society’s (FLHS) website. We hope that you enjoy looking at the material which has been
-                                added to the site so far, but please remember, the website is a work in progress and
-                                will only get bigger and better as time goes on. Please remember too, it is also your
-                                website and we’ll be happy to listen to any feedback, suggestions and ideas for new
-                                sections, although some may already be works in progress.
+                                We hope that you enjoy looking at the material which has been added to the site so far, but please remember, the website will always be a work in progress and will only get bigger and better as time goes on. Please remember, it is also your website and we’ll be happy to listen to any feedback, suggestions and ideas for new sections, for future inclusion on the site. Some may already be works in progress.
                             </p>
                             <p>
-                                It is important to understand that much of the material that we place on the website has
-                                little by the way of donor credits or in many cases identification. Some picture
-                                captions we have added may be incorrect but we will be more than happy for those viewing
-                                the website who know better than we, to advise us what to change, to make the content
-                                more accurate.
+                                It is important to understand that much of the material that we place on the website has little by the way of donor credits or in many cases identification. Some picture captions we have added may be incorrect but we will be more than happy for those viewing the website who know better than we, to advise us what to change, to make the content more accurate.
                             </p>
-                            <div className={styles.actions}>
-                                <Button to="/news/71VVJvH3MQZs1W0k6NWFPS" secondary>
-                                    Read more
-                                </Button>
-                            </div>
+                            <p>
+                                Keep dipping into the website regularly as new items are added all the time and most of all, enjoy discovering Finedon’s historical heritage.
+                            </p>
                         </div>
                         <div className={styles.featureImage}>
                             <img
